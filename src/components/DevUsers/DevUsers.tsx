35faaef60e331fc6e46c2f84e37eb1dfca9bbf8c
itrostik <RosASergeev@stud.kpfu.ru>
2024-03-27 12:00:26 +0300

    фикс неточностей

--- src/components/DevUsers/DevUsers.tsx
+++ src/components/DevUsers/DevUsers.tsx
@@ -3,9 +3,16 @@ import { useMutation, useQuery } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 
 import { Doc, Id } from "../../../convex/_generated/dataModel";
+import { Chat } from "@/interfaces/Chat";
+import { Dialog } from "@/interfaces/Dialog";
 
-//пока не типизировал chats
-export default function DevUsers({ user, chats }: { user: Doc<"user">; chats: any[] }) {
+export default function DevUsers({
+  user,
+  chats,
+}: {
+  user: Doc<"user">;
+  chats: Chat[];
+}) {
   const users = useQuery(api.user.devGetAll);
 
   const dialogCreateMutation = useMutation(api.dialog.create);
@@ -26,7 +33,12 @@ export default function DevUsers({ user, chats }: { user: Doc<"user">; chats: an
         users?.map((userItem) => (
           <div key={userItem._id}>
             {userItem._id !== user._id &&
-              !chats.find((chat) => chat.chat.user.id === userItem._id) && (
+              !chats.find((chat) => {
+                if (chat.type === "dialog") {
+                  let dialog = chat as Dialog;
+                  return dialog.chat.user._id === userItem._id;
+                }
+              }) && (
                 <button onClick={() => createDialog(userItem._id)}>
                   {userItem.name}
                 </button>
