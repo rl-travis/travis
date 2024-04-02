efa730a393918a09adea1a1670adfa75190ded07
itrostik <RosASergeev@stud.kpfu.ru>
2024-04-02 12:15:00 +0300

    подрубаем zustand

---
+++ src/app/_store/zustand.ts
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { ChatType } from "@/5.entities";
+import { Doc } from "../../../convex/_generated/dataModel";
+
+type Store = {
+  chat: null | ChatType;
+  open: (chat: ChatType) => void;
+  close: () => void;
+  user: Doc<"user"> | null;
+  setUser: (user: Doc<"user"> | null) => void;
+};
+
+export const useStore = create<Store>()((set) => ({
+  chat: null,
+  open: (chat) => set(() => ({ chat: chat })),
+  close: () => set(() => ({ chat: null })),
+  user: null,
+  setUser: (user) => set(() => ({ user: user })),
+}));
