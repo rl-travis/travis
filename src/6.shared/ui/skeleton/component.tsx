e72c6fa70080279f5a2550ffde154a966be9659d
itrostik <RosASergeev@stud.kpfu.ru>
2024-04-02 11:15:58 +0300

    сделан скелетон для сообщений

--- src/6.shared/ui/skeleton/component.tsx
+++ src/6.shared/ui/skeleton/component.tsx
@@ -42,3 +42,140 @@ export function SkeletonChat() {
     </Skeleton>
   );
 }
+
+type SkeletonMessageType = {
+  width: number;
+  isGroup: boolean;
+  isReply: boolean;
+};
+
+export function SkeletonMessage({ width, isGroup, isReply }: SkeletonMessageType) {
+  return (
+    <>
+      {isGroup && !isReply && (
+        <Skeleton>
+          <div
+            style={{
+              maxWidth: width,
+              width: "100%",
+              height: 90,
+              borderRadius: 5,
+              padding: 10,
+              gap: 10,
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <div
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: "50%",
+              }}
+            />
+            <div
+              style={{
+                flex: 1,
+                borderRadius: 10,
+                height: 70,
+              }}
+            ></div>
+          </div>
+        </Skeleton>
+      )}
+      {isReply && !isGroup && (
+        <Skeleton>
+          <div
+            style={{
+              maxWidth: width,
+              width: "100%",
+              height: 90,
+              borderRadius: 5,
+              padding: 10,
+              gap: 10,
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <div
+              style={{
+                flex: 1,
+                borderRadius: 10,
+                height: 70,
+                padding: 10,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  flex: 1,
+                  borderRadius: 10,
+                  height: 30,
+                }}
+              ></div>
+            </div>
+          </div>
+        </Skeleton>
+      )}
+      {isReply && isGroup && (
+        <Skeleton>
+          <div
+            style={{
+              maxWidth: width,
+              width: "100%",
+              height: 90,
+              borderRadius: 5,
+              padding: 10,
+              gap: 10,
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          >
+            <div
+              style={{
+                width: 40,
+                height: 40,
+                borderRadius: "50%",
+              }}
+            />
+            <div
+              style={{
+                flex: 1,
+                borderRadius: 10,
+                height: 70,
+                padding: 10,
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <div
+                style={{
+                  flex: 1,
+                  borderRadius: 10,
+                  height: 30,
+                }}
+              ></div>
+            </div>
+          </div>
+        </Skeleton>
+      )}
+      {!isReply && !isGroup && (
+        <Skeleton>
+          <div
+            style={{
+              maxWidth: width,
+              width: "100%",
+              height: 90,
+              borderRadius: 5,
+              padding: 10,
+              gap: 10,
+              display: "flex",
+              alignItems: "flex-end",
+            }}
+          ></div>
+        </Skeleton>
+      )}
+    </>
+  );
+}
