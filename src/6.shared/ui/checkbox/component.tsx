1877cbcfef2962bdf61a94310999a748ed23e8cc
itrostik <rostislavsergeev.it@gmail.com>
2024-04-23 20:11:05 +0300

    чекбокс для выбора языка

---
+++ src/6.shared/ui/checkbox/component.tsx
@@ -0,0 +1,10 @@
+import styles from "./component.module.scss";
+import { Check } from "lucide-react";
+
+export function Checkbox() {
+  return (
+    <div className={styles.wrapper}>
+      <Check size={20} color={"#fff"} />
+    </div>
+  );
+}
