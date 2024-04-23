4d93c21d23d89720df81c06f36cd47d606a16f32
itrostik <rostislavsergeev.it@gmail.com>
2024-04-23 20:12:21 +0300

    закрытие настроек в store useSettingsStore

--- src/6.shared/lib/store/settingsStore.ts
+++ src/6.shared/lib/store/settingsStore.ts
@@ -1,6 +1,9 @@
 import { create } from "zustand";
 
 type SettingsStore = {
+  isOpen: boolean;
+  open: () => void;
+  close: () => void;
   isProfile: boolean;
   isLanguage: boolean;
   openProfile: () => void;
@@ -12,6 +15,9 @@ type SettingsStore = {
 export const useSettingsStore = create<SettingsStore>()((set) => ({
   isProfile: false,
   isLanguage: false,
+  isOpen: false,
+  open: () => set(() => ({ isOpen: true })),
+  close: () => set(() => ({ isOpen: false })),
   openProfile: () => set(() => ({ isProfile: true })),
   closeProfile: () => set(() => ({ isProfile: false })),
   openLanguage: () => set(() => ({ isLanguage: true })),
